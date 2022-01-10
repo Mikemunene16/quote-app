9fb517c52bbbe6dd58a589e96858c5bc4ef94602
Mike Munene <mike.munene@moringaschool.com>
2022-01-10 23:30:54 +0300

    Create a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DataPipePipe } from './data-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuotesDetailComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DataPipePipe
   ],
   imports: [
     BrowserModule,
